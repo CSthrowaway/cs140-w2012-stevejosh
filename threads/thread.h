5a99f3ebadad14e85e18bd677dc64ed8ffb58c4f
XeonXT1 <xeonxt1@gmail.com>
2012-01-21 01:35:45 +0000

    Fixing thread.c so that the sleep queue now works as intended. This version passes alarm-single and alarm-multiple.

--- threads/thread.h
+++ threads/thread.h
@@ -129,7 +129,7 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
-void thread_sleep(int64_t wakeuptime);
+void thread_sleep(int64_t wake_time);
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
