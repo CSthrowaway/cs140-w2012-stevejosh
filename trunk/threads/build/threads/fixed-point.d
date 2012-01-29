990b72a7175f997da0fdaaaa3ceee314f249c99e
xeonxt1@gmail.com <xeonxt1@gmail.com@c67acf2c-f9b1-e500-3610-539e7a20d5eb>
2012-01-29 22:25:54 +0000

    [STABLE] (7) FUCK YOU SVN.

--- trunk/threads/build/threads/fixed-point.d
+++
@@ -1,4 +0,0 @@
-threads/fixed-point.o: ../../threads/fixed-point.c \
-  ../../threads/fixed-point.h ../../lib/stdio.h ../../lib/debug.h \
-  ../../lib/stdarg.h ../../lib/stdbool.h ../../lib/stddef.h \
-  ../../lib/stdint.h ../../lib/kernel/stdio.h
