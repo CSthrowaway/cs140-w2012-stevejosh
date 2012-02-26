8d6fc5154c6f069aaebd9bb75d035d8b352cdda1
sklesser <sklesser@c67acf2c-f9b1-e500-3610-539e7a20d5eb>
2012-02-26 02:28:01 +0000

    mmap syscall stuff and thread mmap_table update

--- trunk/userprog/process.h
+++ trunk/userprog/process.h
@@ -3,11 +3,24 @@
 
 #include "threads/thread.h"
 
+#ifdef VM
+struct mmap_table_entry
+{
+  struct list_elem elem;
+  mapid_t id;
+  int fd;
+};
+
+#endif
+
 tid_t process_execute (const char *file_name);
 void process_init (void);
 void process_release (int exit_code);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
+int process_add_mmap_from_name (const char *file_name);
+int process_add_mmap_from_fd (int fd);
 
 #endif /* userprog/process.h */
